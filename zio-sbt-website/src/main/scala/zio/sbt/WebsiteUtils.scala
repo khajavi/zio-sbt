c7761674e854a1034a26ce95a1bf9c35850e4808
Milad Khajavi <khajavi@gmail.com>
2023-02-06 14:12:34 +0330

    run build workflow on all events.

--- zio-sbt-website/src/main/scala/zio/sbt/WebsiteUtils.scala
+++ zio-sbt-website/src/main/scala/zio/sbt/WebsiteUtils.scala
@@ -298,10 +298,7 @@ object WebsiteUtils {
           jobs = Seq(
             Job(
               id = "build",
-              name = "Build and Test",
-              condition = Some(
-                Condition.Expression("github.event_name == 'pull_request'")
-              ),
+              name = "Build",
               steps = Seq(
                 Step.StepSequence(
                   checkArtifactBuildProcess match {
